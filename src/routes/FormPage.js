8813c63cd9926c891f4e6b88ae5ada0f109ba559
pferlito <pferlito@gmail.com>
2019-10-09 11:33:34 -0700

    Continue work on form page.

--- src/routes/FormPage.js
+++ src/routes/FormPage.js
@@ -1,14 +1,76 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 
+function BillingForm({state,handleChange}) {
+
+  return (
+    <form>
+      <Row>
+        <Col md={6} className="mb-3">
+          <label htmlFor="firstName">First name</label>
+          <input type="text" className="form-control" id="firstName"
+                 onChange={handleChange}
+                 placeholder=""
+                 value={state.firstName}/>
+        </Col>
+        <Col md={6} className="mb-3">
+          <label htmlFor="lastName">Last name</label>
+          <input type="text" className="form-control" id="lastName"
+                 onChange={handleChange}
+                 placeholder=""
+                 value={state.lastName}/>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="mb-3">
+          <label htmlFor="email">Email</label>
+          <input type="email" className="form-control" id="email"
+                 onChange={handleChange}
+                 placeholder=""
+                 value={state.email}/>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="mb-3">
+          <label htmlFor="address">Address</label>
+          <input type="text" className="form-control" id="address"
+                 onChange={handleChange}
+                 placeholder=""
+                 value={state.address}/>
+        </Col>
+      </Row>
+    </form>
+  );
+}
+
 function FormPage() {
-  return(
+  const [state, setState] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    address: ''
+  });
+
+  function handleChange(e) {
+    setState({...state, [e.target.id]: e.target.value});
+  }
+
+  return (
     <Container>
       <Row>
+        <Col md={{span: 8, order: 1}}>
+          <h4 className="mb-3">Billing Address</h4>
+          <BillingForm
+            state={state}
+            handleChange={handleChange}
+          />
+        </Col>
+      </Row>
+      <Row>
         <Col>
-          <p>Example of using a form.</p>
+          {JSON.stringify(state)}
         </Col>
       </Row>
     </Container>
