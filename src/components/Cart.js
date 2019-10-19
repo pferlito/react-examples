7aa54d091b7798fc049d039925cf75dc24eea6c6
pferlito <pferlito@gmail.com>
2019-10-19 05:53:21 -0700

    Work on quantity.

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,22 +2,44 @@ import React, {useState, useEffect} from "react";
 import Col from "react-bootstrap/Col";
 import {PromoDisplay, PromoForm} from "./Promo";
 
-function LineItem({idx,item,handleItemRemove}) {
+function Quantity({qty, handleQtyChange}) {
+  let options = [];
+  for (let i = 1; i < 10; i++) {
+    options.push(<option key={`qty-${i}`}>{i}</option>);
+  }
+  return (
+    <select className="qty-select custom-select" onChange={handleQtyChange} value={qty}>
+      {options}
+    </select>
+  );
+}
+
+function LineItem({idx, item, handleItemRemove, handleQtyChange}) {
+  function QtyChange(e) {
+    handleQtyChange(idx,e.target.value);
+  }
+
   return (
     <li
       className="list-group-item lh-condensed">
       <div className="wrap d-flex justify-content-between">
-        <div className="item-wrap">
+        <div className="cart-item-wrap">
           <h6 className="my-0">{item.name}</h6>
           <small className="text-muted">{item.description}</small>
         </div>
-        <span className="text-muted">${item.price}</span>
+        <Quantity
+          qty={item.qty}
+          handleQtyChange={QtyChange}
+        />
+        <span className="cart-price text-muted">${item.price}</span>
       </div>
       <div>
-        <small><button
-          data-idx={idx}
-          onClick={handleItemRemove}
-          className="link-button">Remove</button>
+        <small>
+          <button
+            data-idx={idx}
+            onClick={handleItemRemove}
+            className="link-button">Remove
+          </button>
         </small>
       </div>
     </li>
@@ -34,23 +56,31 @@ function Cart() {
     {
       name: 'Product 1',
       description: 'Product 1 description',
+      qty: 1,
       price: 18
     },
     {
       name: 'Product 2',
       description: 'Product 2 description',
+      qty: 2,
       price: 22
     },]);
 
   function handleItemRemove(e) {
-    console.log('removing');
     const index = e.target.dataset.idx;
     const newCart = [...cart];
-    newCart.splice(index,1);
+    newCart.splice(index, 1);
     setCart(newCart);
   }
 
-  let total = useEffect(() => {},[cart]);
+  function handleQtyChange(index,qty) {
+    const newCart = [...cart];
+    newCart[index].qty = qty;
+    setCart(newCart);
+  }
+
+  let total = useEffect(() => {
+  }, [cart]);
   return (
     <Col md={{span: 4, order: 2}}>
       <h4 className="d-flex justify-content-between align-items-center mb-3">
@@ -64,6 +94,7 @@ function Cart() {
             key={index}
             item={item}
             handleItemRemove={handleItemRemove}
+            handleQtyChange={handleQtyChange}
           />
         })}
         <PromoDisplay promo={promo} setPromo={setPromo}/>
