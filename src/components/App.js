df18bd4a9daef2fca335b82989b634a3161c6083
pferlito <pferlito@gmail.com>
2019-10-22 18:10:22 -0700

    Work on home page.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Nav from "react-bootstrap/Nav";
 import './App.css';
 
 function App() {
   return (
-    <div className="Home">
-      <h2 className="text-center">Home Page</h2>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <div className="Home">
+            <h2 className="text-center">React Examples</h2>
+            <dl>
+              <dt><a href="/modal">Modal</a></dt>
+              <dd>A Modal implementation using a Portal.</dd>
+              <dt><a href="/checkout">Checkout Form</a></dt>
+              <dd>A Checkout Form for an ecommerce site.</dd>
+            </dl>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
