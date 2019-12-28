b15f7040df3d92e840dad4d432af8549c17bf6ed
pferlito <pferlito@gmail.com>
2019-12-28 08:37:51 -0800

    Begin pagination.

--- src/routes/DirectoryPage.js
+++ src/routes/DirectoryPage.js
@@ -1,31 +1,40 @@
-import React, {useState,useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
+import Pagination from "react-bootstrap/Pagination";
 import DirectoryView from "../components/directory/DirectoryView";
 
 /**
  * Display a directory search page.
  */
 function DirectoryPage() {
-  let [users,setUsers] = useState([]);
-  let [search,setSearch] = useState("");
+  let [users, setUsers] = useState([]);
+  let [search, setSearch] = useState("");
+  let [currentPage, setCurrentPage] = useState (1);
 
-  function handleChange(e) {
-    setSearch(e.target.value);
+  function handleSearch(e) {
+    setSearch(e.target);
+  }
+  
+  function handlePageChange(e) {
+    let newPage = parseInt(e.target.getAttribute('page'));
+    console.log(newPage);
+    setCurrentPage(newPage);
   }
 
-  useEffect( ()=>{
+  useEffect(() => {
     async function getData() {
-      let response = await fetch('https://randomuser.me/api/?seed=pferlito&results=50&inc=name,login,email,picture');
+      let response = await fetch('https://randomuser.me/api/?seed=pferlito&results=5&inc=name,login,email,picture');
       //let response = await fetch('https://randomuser.me/api/?results=50');
       let data = await response.json();
       data = data.results;
       setUsers(data);
     }
+
     getData();
-  },[]);
+  }, []);
 
   return (
     <Container>
@@ -33,7 +42,8 @@ function DirectoryPage() {
         <Col md={{span: 8}}>
           <Form>
             <Form.Group controlId="formBasicEmail">
-              <Form.Control onChange={handleChange} type="text" placeholder="Search" />
+              <Form.Control onChange={handleSearch} type="text"
+                            placeholder="Search"/>
             </Form.Group>
           </Form>
         </Col>
@@ -41,6 +51,15 @@ function DirectoryPage() {
       <Row>
         <Col md={{span: 8}}>
           <DirectoryView users={users} search={search}/>
+        </Col>
+      </Row>
+      <Row className="justify-content-md-center">
+        <Col md={{span: 3}}>
+          <Pagination>
+            <Pagination.Item page={1} onClick={handlePageChange} key={1} active={currentPage === 1}>1</Pagination.Item>
+            <Pagination.Item page={2} onClick={handlePageChange} key={2} active={currentPage === 2}>2</Pagination.Item>
+            <Pagination.Item page={3} onClick={handlePageChange} key={3} active={currentPage === 3}>3</Pagination.Item>
+          </Pagination>
         </Col>
       </Row>
     </Container>
