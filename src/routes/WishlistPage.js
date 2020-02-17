586b699306104fa8bc8b81936adc58aefeeedea5
pferlito <pferlito@gmail.com>
2020-02-17 05:38:17 -0800

    Code factoring.

--- src/routes/WishlistPage.js
+++ src/routes/WishlistPage.js
@@ -7,25 +7,41 @@ export default function WishlistPage() {
     console.log('on drag end');
   }
 
+  const initial = Array.from({ length: 10 }, (v, k) => k).map(k => {
+    return {
+      id: `id-${k}`,
+      content: `Item ${k}`
+    };
+  });
+
+  console.log(initial);
+
+  function WishListItem({ item, index }) {
+    return (
+      <Draggable draggableId={item.id} index={index}>
+        {provided => (
+          <div
+            ref={provided.innerRef}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+          >
+            {item.content}
+          </div>
+        )}
+      </Draggable>
+    );
+  }
+
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <Droppable droppableId="list">
-        {(provided, snapshot) => (
+        {(provided) => (
           <div ref={provided.innerRef}
           {...provided.draggableProps}
           {...provided.dragHandleProps}
           >
-            <Draggable draggableId="test" index={0}>
-              {provided => (
-                <div
-                  ref={provided.innerRef}
-                  {...provided.draggableProps}
-                  {...provided.dragHandleProps}
-                >
-                  Test Content 1
-                </div>
-              )}
-            </Draggable>
+            <WishListItem item={{id: 'item-0', content: 'Item 0'}} index={0}/>
+            <WishListItem item={{id: 'item-1', content: 'Item 1'}} index={1}/>
             {provided.placeholder}
           </div>
           )}
